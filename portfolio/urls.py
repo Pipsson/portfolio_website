aefbcb7a0b46d20bcd6e705c5bc95ea07246d54d
Pipsson <juliusmallya8420@gmail.com>
2022-04-15 15:12:01 +0300

    changes on tabs and addition of start parallax

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from blog import urls
+from django.conf.urls.static  import  static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls'))
-]
+]+static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
