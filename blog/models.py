22d81606292200335c2d9c6993306f2c1df85cc0
Pipsson <juliusmallya8420@gmail.com>
2022-04-16 15:21:40 +0300

    changes on models

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,15 @@
 from email.mime import image
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
 class Portfolio (models.Model):
     image = models.ImageField()
+    name = models.CharField(max_length= 200)
     discription = models.TextField(max_length= 300)
     link1 = models.URLField( blank= True)
     link2 = models.URLField(blank= True)
+
+    def __str__(self):
+           return self.name
+    
