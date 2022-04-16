64670dd00051868ed18de87dd3e550f8cba3bd03
Pipsson <juliusmallya8420@gmail.com>
2022-04-16 15:16:39 +0300

    changes on models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
+from email.mime import image
 from django.db import models
 
 # Create your models here.
+class Portfolio (models.Model):
+    image = models.ImageField()
+    discription = models.TextField(max_length= 300)
+    link1 = models.URLField( blank= True)
+    link2 = models.URLField(blank= True)
