a5a209aa82cafbd1ab8d0861b1e881ed3d462cf9
Pipsson <juliusmallya8420@gmail.com>
2022-04-16 15:48:22 +0300

    deployment

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Portfolio (models.Model):
     image = models.ImageField()
-    name = models.CharField(max_length= 200)
+    name = models.CharField(max_length= 200, default= 'biteschool')
     discription = models.TextField(max_length= 300)
     link1 = models.URLField( blank= True)
     link2 = models.URLField(blank= True)
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+import django_heroku
+import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware' # added during deployment
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
@@ -115,10 +118,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATICFILES_STORAGE = 'Whitenoise.storage.CompressedManifestStaticFilesStrorage'  #deployment
 STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 STATICFILES_DIRS = [BASE_DIR , 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
+
+django_heroku.settings(locals())  #deployment
